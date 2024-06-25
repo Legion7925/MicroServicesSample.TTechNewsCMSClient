1e33d7511225221d5d809b2f1d05c230233e8d15
Amim7925 <amim.464432@gmail.com>
2024-06-25 16:45:56 +0330

    added elastic search log

--- TTechCMSApiGateway/Program.cs
+++ TTechCMSApiGateway/Program.cs
@@ -1,14 +1,51 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 using Steeltoe.Discovery.Client;
 using TTechCMSApiGateway.Extentsions;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDiscoveryClient();
-builder.Services.AddReverseProxy()
-    .LoadFromEureka(builder.Services);
-//.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-var app = builder.Build();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up");
 
-app.MapReverseProxy();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((ctx, lc) => lc
+ .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+   .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+   {
+       DetectElasticsearchVersion = false,
+       AutoRegisterTemplate = true,
+       AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+       IndexFormat = "ttech-apigateway-index-{0:yyyy.MM}",
+       MinimumLogEventLevel = Serilog.Events.LogEventLevel.Debug
+   })
+ .Enrich.FromLogContext()
+ .ReadFrom.Configuration(ctx.Configuration));
+    builder.Services.AddDiscoveryClient();
+    builder.Services.AddHealthChecks();
+    builder.Services.AddReverseProxy()
+        .LoadFromEureka(builder.Services);
+    //.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    var app = builder.Build();
 
+    app.MapReverseProxy();
+    app.UseRouting();
 
-app.Run();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapHealthChecks("/health/live");
+    });
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
--- TTechNewsCMSClient/Program.cs
+++ TTechNewsCMSClient/Program.cs
@@ -1,35 +1,71 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddHttpClient("bi" , c =>
-{
-    c.BaseAddress = new Uri("http://localhost:7300/bi/");
-});
-builder.Services.AddHttpClient("news" , c =>
-{
-    c.BaseAddress = new Uri("http://localhost:7300/news/");
-});
 
-var app = builder.Build();
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up");
+
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+  .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+  {
+      DetectElasticsearchVersion = false,
+      AutoRegisterTemplate = true,
+      AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+      IndexFormat = "ttech-newscmsclient-index-{0:yyyy.MM}",
+      MinimumLogEventLevel = Serilog.Events.LogEventLevel.Debug
+  })
+   .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+   .Enrich.FromLogContext()
+   .ReadFrom.Configuration(ctx.Configuration));
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddHttpClient("bi", c =>
+    {
+        c.BaseAddress = new Uri("http://localhost:7300/bi/");
+    });
+    builder.Services.AddHttpClient("news", c =>
+    {
+        c.BaseAddress = new Uri("http://localhost:7300/news/");
+    });
+    builder.Services.AddHealthChecks();
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+    app.MapHealthChecks("/health/live");
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
